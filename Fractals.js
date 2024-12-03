0394082af2dde5b0867114d333aaa4009c487faf
Jessica Suarez <jessicarsuarez14@gmail.com>
2024-12-03 09:17:01 -0500

    Update Fractals.js

--- Fractals.js
+++ Fractals.js
@@ -9,12 +9,8 @@ function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
 
-  document.getElementById( "xButton" ).onclick = function () {
-        axis = xAxis;
-    };
-    document.getElementById( "yButton" ).onclick = function () {
-        axis = yAxis;
-    };
+  document.getElementById( "ButtonTexture" ).onclick = function ();
+    document.getElementById( "ButtonZoom" ).onclick = function ();
 }
 
 // Add fractal here 
