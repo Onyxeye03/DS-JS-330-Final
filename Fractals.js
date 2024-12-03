7f702976a01f1e1ccfd765c7dd92ffdca41c5bd8
Jessica Suarez <jessicarsuarez14@gmail.com>
2024-12-03 09:14:55 -0500

    Update Fractals.js
    added the webgl and buttons

--- Fractals.js
+++ Fractals.js
@@ -1,1 +1,20 @@
+//webgl function
+function init() {
+
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+
+  document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+    };
+}
+
 // Add fractal here 
