5f5d071510075dfe1a8652007e7b00d48a9c85cf
Onyxeye03 <129223839+Onyxeye03@users.noreply.github.com>
2024-12-03 09:59:28 -0500

    Update Fractals.js

--- Fractals.js
+++ Fractals.js
@@ -17,7 +17,8 @@ function init() {
 gl.viewport(0, 0, canvas.width, canvas.height);
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
 // Load shaders and initialize attribute buffers
-        var program = initShaders(gl, vertex-shader", "fragment-shader"); gl.useProgram(program);
+        var program = initShaders(gl, vertex-shader", "fragment-shader"); 
+        gl.useProgram(program);
         // Load the data into the GPU
         var bufferId = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
