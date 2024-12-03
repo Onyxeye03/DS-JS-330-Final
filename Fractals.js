1b40e014443a41ceba1224395d13d28fab501314
Onyxeye03 <129223839+Onyxeye03@users.noreply.github.com>
2024-12-03 09:54:19 -0500

    Update Fractals.js

--- Fractals.js
+++ Fractals.js
@@ -14,8 +14,7 @@ function init() {
 //
 // Configure WebGL
 //
-gl.viewport(0, 0, canvas.width,
-            canvas.height);
+gl.viewport(0, 0, canvas.width, canvas.height);
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
 // Load shaders and initialize attribute buffers
         var program = initShaders(gl, vertex-shader", "fragment-shader"); gl.useProgram(program);
